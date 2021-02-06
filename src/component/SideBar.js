7ac6849ddada541354a2a5b2efda255e6be91c38
kumard3 <kumardeepanshu157200@gmail.com>
2021-02-06 15:21:12 +0530

    started

---
+++ src/component/SideBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+function SideBar() {
+    return (
+        <Sidebar>
+        
+
+        </Sidebar>
+    )
+}
+
+export default SideBar
+
+const Sidebar = styled.div `
+width: 10vw;
+height:100vh;
+background-color: black;
+float:left;
+border-right: 1px solid white;
+
+`
